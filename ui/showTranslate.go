9ada56d054c4b0d021cc0927eb37a37f3dfc5199
Ailzr <ailzr@outlook.com>
2025-04-14 12:37:10 +0800

    fix: 修复翻译框遮挡截屏的问题，修复ocr结果判断错误的问题

--- ui/showTranslate.go
+++ ui/showTranslate.go
@@ -26,6 +26,8 @@ func (mw *MainWindow) CreateShowWindow() {
 
 	mw.TranslatorWindow.SetContent(contentShow)
 
+	//TODO 翻译框大小从配置中读取，动态修改写入到配置中保存
+
 	mw.TranslatorWindow.Resize(fyne.NewSize(800, 300))
 	mw.TranslatorWindow.Show()
 }
@@ -39,21 +41,24 @@ func (mw *MainWindow) ShowTranslate(text string) {
 }
 
 func (mw *MainWindow) Translate() {
+	//TODO 将主窗口也隐藏，避免点击翻译按钮时截屏被遮挡
+
 	mw.ShowTranslate("翻译中...")
 	mw.TranslatorWindow.Hide()
+
 	img, err := utils.CaptureImg()
 	utils.SaveImgToPng(img, "tmp")
 	if err != nil {
 		logHelper.Error(err.Error())
 		logHelper.WriteLog(err.Error())
 		mw.ShowTranslate("截图失败")
-		return
 	}
-	mw.TranslatorWindow.Show()
+
 	ocrResult := ocr.GetOCRResult()
-	if ocrResult != "" {
+	if ocrResult == "" {
 		mw.ShowTranslate("未识别到文字")
 	}
+	mw.TranslatorWindow.Show()
 	result := llm.Translate(ocrResult, "简体中文")
 	mw.ShowTranslate(result)
 }
