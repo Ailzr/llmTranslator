d838b010ab7ec4adfe86d9497c228e5b46d81684
Ailzr <ailzr@outlook.com>
2025-04-14 18:26:36 +0800

    feat: 优化截屏功能
    优化截屏背景，添加灰色蒙版，将捕获区域随鼠标动态变化，删去灰色蒙版，让捕获区域显示原色以凸显框选区域，实现类似QQ的截图效果

--- ui/translatorCapture.go
+++ ui/translatorCapture.go
@@ -27,6 +27,12 @@ func (mw *MainWindow) CaptureRectangle() {
 	w.SetFullScreen(true)
 	w.SetPadded(false)
 
+	//截图前将主窗口和翻译窗口隐藏
+	if !mw.isTray {
+		mw.Window.Hide()
+	}
+	mw.TranslatorWindow.Hide()
+
 	bg := canvas.NewImageFromImage(img)
 	bg.FillMode = canvas.ImageFillStretch
 
@@ -42,6 +48,13 @@ func (mw *MainWindow) CaptureRectangle() {
 			w.Close()
 		}
 	})
+
+	//重新显示主窗口和翻译窗口
+	if !mw.isTray {
+		mw.Window.Show()
+	}
+	mw.TranslatorWindow.Show()
+
 	w.Show()
 	go func() {
 		sel := <-result
@@ -73,69 +86,98 @@ func (mw *MainWindow) CaptureRectangle() {
 
 type selectOverlay struct {
 	widget.BaseWidget
+	mask      *canvas.Raster
 	rect      *canvas.Rectangle
 	start     fyne.Position
+	curr      fyne.Position
 	selecting bool
 	result    chan image.Rectangle
 }
 
 func newSelectOverlay(result chan image.Rectangle) *selectOverlay {
 	o := &selectOverlay{
-		rect:   canvas.NewRectangle(color.NRGBA{R: 0, G: 0, B: 255, A: 64}),
+		rect:   canvas.NewRectangle(color.NRGBA{R: 0, G: 255, B: 0, A: 0}), // 先只画边框
 		result: result,
 	}
 	o.rect.StrokeColor = color.NRGBA{R: 0, G: 255, B: 0, A: 255}
 	o.rect.StrokeWidth = 2
-	o.ExtendBaseWidget(o) // 注册为可接收事件的 Widget
-	o.rect.Hide()
+
+	// 蒙版：除了选区内部，全屏都用半透明灰
+	o.mask = canvas.NewRasterWithPixels(func(x, y, w, h int) color.Color {
+		sx, sy := int(o.start.X), int(o.start.Y)
+		cx, cy := int(o.curr.X), int(o.curr.Y)
+		minX, maxX := min(sx, cx), max(sx, cx)
+		minY, maxY := min(sy, cy), max(sy, cy)
+		if x >= minX && x <= maxX && y >= minY && y <= maxY {
+			// 选区内完全透明，露出原始截图
+			return color.NRGBA{0, 0, 0, 0}
+		}
+		// 选区外半透明灰
+		return color.NRGBA{R: 0, G: 0, B: 0, A: 128}
+	})
+
+	o.ExtendBaseWidget(o)
 	return o
 }
 
-func (o *selectOverlay) CreateRenderer() fyne.WidgetRenderer {
-	return &overlayRenderer{objects: []fyne.CanvasObject{o.rect}}
-}
-
 type overlayRenderer struct {
-	objects []fyne.CanvasObject
+	mask *canvas.Raster
+	rect *canvas.Rectangle
 }
 
-func (r *overlayRenderer) Layout(size fyne.Size) {}
-func (r *overlayRenderer) MinSize() fyne.Size    { return fyne.NewSize(0, 0) }
-func (r *overlayRenderer) Refresh() {
-	for _, o := range r.objects {
-		o.Refresh()
-	}
+func (o *selectOverlay) CreateRenderer() fyne.WidgetRenderer {
+	//return &overlayRenderer{objects: []fyne.CanvasObject{o.rect}}
+	return &overlayRenderer{mask: o.mask, rect: o.rect}
 }
-func (r *overlayRenderer) Objects() []fyne.CanvasObject { return r.objects }
-func (r *overlayRenderer) Destroy()                     {}
+
+func (r *overlayRenderer) Layout(size fyne.Size) {
+	// 蒙版铺满
+	r.mask.Resize(size)
+	r.mask.Move(fyne.NewPos(0, 0))
+	// rect 的位置和大小由 overlay 自己在 MouseMoved 里设置
+}
+func (r *overlayRenderer) MinSize() fyne.Size { return fyne.NewSize(0, 0) }
+func (r *overlayRenderer) Refresh() {
+	r.mask.Refresh()
+	r.rect.Refresh()
+}
+func (r *overlayRenderer) Objects() []fyne.CanvasObject {
+	return []fyne.CanvasObject{r.mask, r.rect}
+}
+func (r *overlayRenderer) Destroy() {}
 
 func (o *selectOverlay) MouseDown(e *desktop.MouseEvent) {
 	if e.Button != desktop.MouseButtonPrimary {
 		return
 	}
 	o.start = e.Position
+	o.curr = e.Position
 	o.selecting = true
+
 	o.rect.Move(o.start)
 	o.rect.Resize(fyne.NewSize(0, 0))
 	o.rect.Show()
-	o.Refresh()
-}
 
-func (o *selectOverlay) MouseIn(_ *desktop.MouseEvent) {} // 必须实现
-func (o *selectOverlay) MouseOut()                     {} // 必须实现
+	o.mask.Refresh()
+	o.rect.Refresh()
+}
 
 func (o *selectOverlay) MouseMoved(e *desktop.MouseEvent) {
 	if !o.selecting {
 		return
 	}
+	o.curr = e.Position
+
 	x1, y1 := int(o.start.X), int(o.start.Y)
-	x2, y2 := int(e.Position.X), int(e.Position.Y)
+	x2, y2 := int(o.curr.X), int(o.curr.Y)
 	minX, minY := float32(min(x1, x2)), float32(min(y1, y2))
 	w, h := float32(abs(x2-x1)), float32(abs(y2-y1))
 
 	o.rect.Move(fyne.NewPos(minX, minY))
 	o.rect.Resize(fyne.NewSize(w, h))
-	o.Refresh()
+
+	o.mask.Refresh()
+	o.rect.Refresh()
 }
 
 func (o *selectOverlay) MouseUp(e *desktop.MouseEvent) {
@@ -144,9 +186,12 @@ func (o *selectOverlay) MouseUp(e *desktop.MouseEvent) {
 	}
 	o.selecting = false
 	x1, y1 := int(o.start.X), int(o.start.Y)
-	x2, y2 := int(e.Position.X), int(e.Position.Y)
+	x2, y2 := int(o.curr.X), int(o.curr.Y)
 	o.result <- image.Rect(min(x1, x2), min(y1, y2), max(x1, x2), max(y1, y2))
 }
+
+func (o *selectOverlay) MouseIn(_ *desktop.MouseEvent) {} // 必须实现
+func (o *selectOverlay) MouseOut()                     {} // 必须实现
 
 func min(a, b int) int {
 	if a < b {
